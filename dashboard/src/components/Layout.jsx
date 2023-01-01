3777cfb8ff499555a733ce53da2ed86a814087c0
Cybo-Neutron <cyboneutron@gmail.com>
2023-01-01 14:51:51 +0530

    eslint and prettier setup

---
+++ dashboard/src/components/Layout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SideNav from "./SideNav";
+
+function Layout() {
+  return (
+    <div className="min-h-screen ">
+      <SideNav />
+    </div>
+  );
+}
+
+export default Layout;
